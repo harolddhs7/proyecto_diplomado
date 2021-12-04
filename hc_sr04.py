8861df6dc233a786668828a311ad67d449fbc0ca
harolddhs7 <harolddhs68@gmail.com>
2021-12-04 00:45:17 -0500

    Correción del sensor de nivel

--- hc_sr04.py
+++ hc_sr04.py
@@ -5,30 +5,26 @@ GPIO.setmode(GPIO.BCM)
 def distancia():
     TRIG = 23
     ECHO = 24
-
     GPIO.setup(TRIG, GPIO.OUT)
     GPIO.setup(ECHO, GPIO.IN)
 
     try:
-        while True:
-            GPIO.output(TRIG, False)
-            print('Esperando a que se estabilice el sensor')
-            time.sleep(2)
-        
-            GPIO.output(TRIG, True)
-            time.sleep(0.00001)
-            GPIO.output(TRIG, False)
-        
-            while GPIO.input(ECHO) == 0:
-                pulse_start = time.time()
+        GPIO.output(TRIG, False)
+        time.sleep(2)
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
 
-            while GPIO.input(ECHO) == 1:
-                pulse_end = time.time()
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
         
-            pulse_duration = pulse_end - pulse_start
-            distance = pulse_duration * 17150
-            distance = round(distance, 2)
-            return distance
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        return distance
 
     except KeyboardInterrupt:
         print("cleaning up!")
